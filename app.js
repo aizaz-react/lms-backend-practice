97db571928e59ab454b35b1474b243f5ebc3a6be
aizaz-react <aizaz9790@gmail.com>
2021-11-27 12:54:15 +0500

    heroku deployment

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require("cors")());
+require("./prod")(app);
 app.use("/api/students", studentsRoutes);
 
 app.listen(PORT, () => console.log("listening on port " + PORT));
